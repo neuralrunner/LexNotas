a0c8f698af21c45c86da8f6f2ac734cf4d7c934d
neuralrunner <brunohcagnin@gmail.com>
2020-07-08 05:27:00 -0300

    Iniciando o Projeto.

---
+++ app/src/main/java/codes/neuralkatana/lexnotas/ui/activity/ListaNotasActivity.java
@@ -0,0 +1,16 @@
+package codes.neuralkatana.lexnotas.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import codes.neuralkatana.R;
+
+public class ListaNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+    }
+}
